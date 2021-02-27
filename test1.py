cd962e0e928667cf93fa3848fc281355a5637e9e
ghispelletier <ghispelletier@gmail.com>
2021-02-27 10:57:33 -0500

    commit initial

---
+++ test1.py
@@ -0,0 +1,8 @@
+x = 2
+y = 10
+
+print("{} + {} = {}".format(x, y, x + y))
+print("{} - {} = {}".format(x, y, x - y))
+print("{} * {} = {}".format(x, y, x * y))
+print("{} / {} = {}".format(x, y, x / y))
+print("{} ** {} = {}".format(x, y, x ** y))
