680c5bd78f0ac40b678dec1dedd9db92807f5cec
ghispelletier <ghispelletier@gmail.com>
2021-02-27 11:16:04 -0500

    Ajouter le logarithme

--- test1.py
+++ test1.py
@@ -1,3 +1,5 @@
+import math
+
 x = 2
 y = 10
 
@@ -6,3 +8,5 @@ print("{} - {} = {}".format(x, y, x - y))
 print("{} * {} = {}".format(x, y, x * y))
 print("{} / {} = {}".format(x, y, x / y))
 print("{} ** {} = {}".format(x, y, x ** y))
+print("log({}) = {}".format(x, math.log(x, 10)))
+print("log({}) = {}".format(y, math.log(y, 10)))
